b482ad2ffa5d64a71965e1383ed5ae7d83b2ee42
ExtraPolite <imextrapolite2@gmail.com>
2025-01-06 15:48:40 -0500

    check that onClick works

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import GetCharacters from './Components/CallApi';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [clickImage, setClick] = useState('');
+
+  const handleClick = (e) => {
+    setClick(e.target.textContent);
+
+    if (clickImage === e.target.textContent) {
+      setScore((prevNum) => prevNum + 1);
+    }
+  };
+
   return (
     <>
-      <GetCharacters />
+      <div>{score}</div>
+      <GetCharacters onClick={handleClick} />
     </>
   );
 }
